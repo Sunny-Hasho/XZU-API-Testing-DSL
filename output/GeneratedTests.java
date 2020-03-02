a0a355726664e7b5a6848b600c47abfa9961c79f
Sunny-Hasho <sandeep.hasarinda@gmail.com>
2020-03-02 15:30:00 +0530

    Finalized Version

--- output/GeneratedTests.java
+++ output/GeneratedTests.java
@@ -7,7 +7,7 @@ import java.nio.charset.StandardCharsets;
 import java.util.*;
 
 public class GeneratedTests {
-    static String BASE = "http://localhost:8080";
+    static String BASE = "";
     static Map<String, String> DEFAULT_HEADERS = new HashMap<>();
     static HttpClient client;
 
@@ -16,14 +16,13 @@ public class GeneratedTests {
         client = HttpClient.newBuilder()
             .connectTimeout(Duration.ofSeconds(5))
             .build();
-        DEFAULT_HEADERS.put("Content-Type", "application/json");
     }
 
     @Test
-    void test_LoginWithMultiline() throws Exception {
+    void test_Login() throws Exception {
         HttpRequest.Builder b = HttpRequest.newBuilder(URI.create(BASE + "/api/login"))
             .timeout(Duration.ofSeconds(10))
-            .POST(HttpRequest.BodyPublishers.ofString("\r\n    {\r\n      \"username\": \"admin\",\r\n      \"password\": \"1234\"\r\n    }\r\n    ", StandardCharsets.UTF_8));
+            .POST(HttpRequest.BodyPublishers.ofString("200", StandardCharsets.UTF_8));
         for (var e : DEFAULT_HEADERS.entrySet()) {
             b.header(e.getKey(), e.getValue());
         }
@@ -32,35 +31,6 @@ public class GeneratedTests {
         assertEquals(200, resp.statusCode());
         assertTrue(resp.headers().firstValue("Content-Type").orElse("").contains("json"));
         assertTrue(resp.body().contains("\"token\":"));
-    }
-
-    @Test
-    void test_GetUserWithRange() throws Exception {
-        HttpRequest.Builder b = HttpRequest.newBuilder(URI.create(BASE + "/api/users/42"))
-            .timeout(Duration.ofSeconds(10))
-            .GET();
-        for (var e : DEFAULT_HEADERS.entrySet()) {
-            b.header(e.getKey(), e.getValue());
-        }
-        HttpResponse<String> resp = client.send(b.build(), HttpResponse.BodyHandlers.ofString(StandardCharsets.UTF_8));
-
-        assertTrue(resp.statusCode() >= 200 && resp.statusCode() <= 299);
-        assertTrue(resp.body().contains("\"id\":42"));
-        assertTrue(resp.body().contains("\"username\":"));
-    }
-
-    @Test
-    void test_UpdateUser() throws Exception {
-        HttpRequest.Builder b = HttpRequest.newBuilder(URI.create(BASE + "/api/users/42"))
-            .timeout(Duration.ofSeconds(10))
-            .PUT(HttpRequest.BodyPublishers.ofString("\r\n    {\r\n      \"role\": \"ADMIN\",\r\n      \"updated\": true\r\n    }\r\n    ", StandardCharsets.UTF_8));
-        for (var e : DEFAULT_HEADERS.entrySet()) {
-            b.header(e.getKey(), e.getValue());
-        }
-        HttpResponse<String> resp = client.send(b.build(), HttpResponse.BodyHandlers.ofString(StandardCharsets.UTF_8));
-
-        assertEquals(200, resp.statusCode());
-        assertTrue(resp.body().contains("\"updated\":"));
     }
 
 }
